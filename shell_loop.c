15c7e6cf90a0de084db19cf6e64dddf1bb85757f
Onyinyechi Gilbert <gilbertonyinyechi1@gmail.com>
2023-02-24 05:07:23 -0800

    shell updated

--- shell_loop.c
+++ shell_loop.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
 * hsh - main shell loop
 * @info: the parameter & return info struct
@@ -10,6 +11,7 @@ int hsh(info_t *info, char **av)
 {
 ssize_t r = 0;
 int builtin_ret = 0;
+
 while (r != -1 && builtin_ret != -2)
 {
 clear_info(info);
@@ -40,14 +42,12 @@ exit(info->err_num);
 }
 return (builtin_ret);
 }
+
 /**
 * find_builtin - finds a builtin command
 * @info: the parameter & return info struct
 *
 * Return: -1 if builtin not found,
-* 0 if builtin executed successfully,
-* 1 if builtin found but not successful,
-* 2 if builtin signals exit(),
 * 0 if builtin executed successfully,
 * 1 if builtin found but not successful,
 * 2 if builtin signals exit()
@@ -56,7 +56,9 @@ int find_builtin(info_t *info)
 {
 int i, built_in_ret = -1;
 builtin_table builtintbl[] = {
-{"exit", _myexit}, {"env", _myenv}, {"help", _myhelp},
+{"exit", _myexit},
+{"env", _myenv},
+{"help", _myhelp},
 {"history", _myhistory},
 {"setenv", _mysetenv},
 {"unsetenv", _myunsetenv},
@@ -64,6 +66,7 @@ builtin_table builtintbl[] = {
 {"alias", _myalias},
 {NULL, NULL}
 };
+
 for (i = 0; builtintbl[i].type; i++)
 if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
 {
@@ -73,6 +76,7 @@ break;
 }
 return (built_in_ret);
 }
+
 /**
 * find_cmd - finds a command in PATH
 * @info: the parameter & return info struct
@@ -83,6 +87,7 @@ void find_cmd(info_t *info)
 {
 char *path = NULL;
 int i, k;
+
 info->path = info->argv[0];
 if (info->linecount_flag == 1)
 {
@@ -94,6 +99,7 @@ if (!is_delim(info->arg[i], " \t\n"))
 k++;
 if (!k)
 return;
+
 path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
 if (path)
 {
@@ -112,6 +118,7 @@ print_error(info, "not found\n");
 }
 }
 }
+
 /**
 * fork_cmd - forks a an exec thread to run cmd
 * @info: the parameter & return info struct
@@ -121,6 +128,7 @@ print_error(info, "not found\n");
 void fork_cmd(info_t *info)
 {
 pid_t child_pid;
+
 child_pid = fork();
 if (child_pid == -1)
 {
--- tokenizer.c
+++ tokenizer.c
@@ -6,10 +6,12 @@
 * @d: the delimeter string
 * Return: a pointer to an array of strings, or NULL on failure
 */
+
 char **strtow(char *str, char *d)
 {
 int i, j, k, m, numwords = 0;
 char **s;
+
 if (str == NULL || str[0] == 0)
 return (NULL);
 if (!d)
@@ -17,6 +19,7 @@ d = " ";
 for (i = 0; str[i] != '\0'; i++)
 if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 numwords++;
+
 if (numwords == 0)
 return (NULL);
 s = malloc((1 + numwords) *sizeof(char *));
@@ -44,6 +47,7 @@ s[j][m] = 0;
 s[j] = NULL;
 return (s);
 }
+
 /**
 * **strtow2 - splits a string into words
 * @str: the input string
@@ -54,6 +58,7 @@ char **strtow2(char *str, char d)
 {
 int i, j, k, m, numwords = 0;
 char **s;
+
 if (str == NULL || str[0] == 0)
 return (NULL);
 for (i = 0; str[i] != '\0'; i++)
@@ -62,11 +67,7 @@ if ((str[i] != d && str[i + 1] == d) ||
 numwords++;
 if (numwords == 0)
 return (NULL);
-<<<<<<< HEAD
-s = malloc((1 + numwords) * sizeof(char *)); 6
-== == == =
-s = malloc((1 + numwords) * sizeof(char *));
->>>>>>> 0e43243acdc6d58ca914331ff09d2b3a82d5b366
+s = malloc((1 + numwords) *sizeof(char *));
 if (!s)
 return (NULL);
 for (i = 0, j = 0; j < numwords; j++)
